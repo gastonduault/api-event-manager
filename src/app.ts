2d6fb13bb794f60fb3bd045ec7e05b3eedfa0343
gaston duault <gas.duault@gmail.com>
2025-02-13 08:43:18 +0100

    FIX setup_prisma with docker

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,10 @@
+import bcrypt from "bcryptjs";
 import express from "express";
+import jwt from "jsonwebtoken";
+import { PrismaClient } from "@prisma/client";
+import { Request, Response, NextFunction } from "express";
+const prisma = new PrismaClient();
+const SECRET_KEY = "secret_key";
 const app = express();
 const port = 3000;
 
@@ -6,6 +12,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.post("/", (req, res) => {
+  res.send("Post work");
+});
+
 app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
+  console.log(`Express is caca listening at http://localhost:${port}`);
 });
