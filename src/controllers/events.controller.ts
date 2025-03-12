cc1cfcd8431a72324b02c40fdce2298a573928b9
Camille Berquet <camille.berquet@united-vr.com>
2025-03-12 16:38:55 +0100

    check the existence of the typeId and responsableId in POST /events

--- src/controllers/events.controller.ts
+++ src/controllers/events.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { EventService } from "../services/events.service";
 import { Event } from "../entities/events.entity";
+import { prisma } from "../prismaClient";
 
 export class EventController {
   static async getEvents(req: Request, res: Response) {
@@ -21,7 +22,54 @@ export class EventController {
           error: "Validation error",
           message: error.message,
         });
+        return;
       }
+
+      const { responsableId, typeId, ...eventData } = req.body;
+
+      //TODO : modify if responsableId or typeId are invalid with the service
+
+      // const { responsableId, typeId } = value;
+      //
+      // const responsable = await ResponsableService.findById(responsableId);
+      // if (!responsable) {
+      //   return res.status(400).json({
+      //     error: "Invalid responsibleId",
+      //     message: `No responsable found with ID ${responsableId}`,
+      //   });
+      // }
+      //
+      // const eventType = await EventTypeService.findById(typeId);
+      // if (!eventType) {
+      //   return res.status(400).json({
+      //     error: "Invalid typeId",
+      //     message: `No event type found with ID ${typeId}`,
+      //   });
+      // }
+      const responsable = await prisma.user.findUnique({
+        where: { id: responsableId },
+      });
+
+      if (!responsable) {
+        res.status(400).json({
+          error: "Invalid responsableId",
+          message: `No user found with ID ${responsableId}`,
+        });
+        return;
+      }
+
+      const eventType = await prisma.type.findUnique({
+        where: { id: typeId },
+      });
+
+      if (!eventType) {
+        res.status(400).json({
+          error: "Invalid typeId",
+          message: `No event type found with ID ${typeId}`,
+        });
+        return;
+      }
+
       const newEvent = await EventService.createEvent(value);
       res.status(201).json(newEvent);
     } catch (error) {
