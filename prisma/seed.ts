df8c0da42d213efc6f4dfd5365c8231c73a6724a
gaston duault <gas.duault@gmail.com>
2025-03-22 15:40:15 +0100

    Feature: add a seed for the db when setup_prisma, default admin user

---
+++ prisma/seed.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const hashedPassword = await bcrypt.hash("admin", 10);
+
+  const adminUser = await prisma.user.upsert({
+    where: { email: "admin@example.com" },
+    update: {},
+    create: {
+      email: "admin@example.com",
+      password: hashedPassword,
+      firstname: "Admin",
+      lastname: "User",
+      isAdmin: true,
+    },
+  });
+
+  console.log("Admin user created:", adminUser);
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
